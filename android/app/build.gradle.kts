e0996bf0c939b92e4de843b220e3febf16c01c34
John Doe <johndoe@example.com>
2026-05-25 23:00:43 +0800

    解决了一些已知的问题

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,7 +57,10 @@ android {
 
     splits {
         abi {
-            isEnable = false
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86_64")
+            isUniversalApk = true
         }
     }
 
